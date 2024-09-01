cdc29e4f642493ccbb042e8b9e3248b34bc9c803
Inefable KOUMBA <iloveyoujulina@gmail.com>
2024-09-01 11:04:25 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#032D47",
+        backgroundLight: "#074166",
+      },
+    },
+  },
+  plugins: [],
+};
